ee75d201fcffbe3e7db24edd15d2e652c6ed7e07
amanpathak05 <pathakaman05@gmail.com>
2015-09-15 14:47:27 +0530

    login page changes

--- sites/all/themes/bartik_c2s/template.php
+++ sites/all/themes/bartik_c2s/template.php
@@ -3,156 +3,126 @@
 /**
  * Add body classes if certain regions have content.
  */
+function bartik_c2s_preprocess_html(&$variables) {
+   //drupal_flush_all_caches();
+    if (!empty($variables['page']['featured'])) {
+        $variables['classes_array'][] = 'featured';
+    }
 
-    
-function bartik_preprocess_html(&$variables) {
-  if (!empty($variables['page']['featured'])) {
-    $variables['classes_array'][] = 'featured';
-  }
+    if (!empty($variables['page']['triptych_first']) || !empty($variables['page']['triptych_middle']) || !empty($variables['page']['triptych_last'])) {
+        $variables['classes_array'][] = 'triptych';
+    }
 
-  if (!empty($variables['page']['triptych_first'])
-    || !empty($variables['page']['triptych_middle'])
-    || !empty($variables['page']['triptych_last'])) {
-    $variables['classes_array'][] = 'triptych';
-  }
+    if (!empty($variables['page']['footer_firstcolumn']) || !empty($variables['page']['footer_secondcolumn']) || !empty($variables['page']['footer_thirdcolumn']) || !empty($variables['page']['footer_fourthcolumn'])) {
+        $variables['classes_array'][] = 'footer-columns';
+    }
 
-  if (!empty($variables['page']['footer_firstcolumn'])
-    || !empty($variables['page']['footer_secondcolumn'])
-    || !empty($variables['page']['footer_thirdcolumn'])
-    || !empty($variables['page']['footer_fourthcolumn'])) {
-    $variables['classes_array'][] = 'footer-columns';
-  }
-
-  // Add conditional stylesheets for IE
-  drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
-  drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
+    // Add conditional stylesheets for IE
+    drupal_add_css(path_to_theme() . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'preprocess' => FALSE));
+    drupal_add_css(path_to_theme() . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
 }
 
 /**
  * Override or insert variables into the page template for HTML output.
  */
-function bartik_process_html(&$variables) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_html_alter($variables);
-  }
-}
 
-/**
- * Override or insert variables into the page template.
- */
-function bartik_process_page(&$variables) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_page_alter($variables);
-  }
-  // Always print the site name and slogan, but if they are toggled off, we'll
-  // just hide them visually.
-  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
-  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
-  if ($variables['hide_site_name']) {
-    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
-    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
-  }
-  if ($variables['hide_site_slogan']) {
-    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
-    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
-  }
-  // Since the title and the shortcut link are both block level elements,
-  // positioning them next to each other is much simpler with a wrapper div.
-  if (!empty($variables['title_suffix']['add_or_remove_shortcut']) && $variables['title']) {
-    // Add a wrapper div using the title_prefix and title_suffix render elements.
-    $variables['title_prefix']['shortcut_wrapper'] = array(
-      '#markup' => '<div class="shortcut-wrapper clearfix">',
-      '#weight' => 100,
-    );
-    $variables['title_suffix']['shortcut_wrapper'] = array(
-      '#markup' => '</div>',
-      '#weight' => -99,
-    );
-    // Make sure the shortcut link is the first item in title_suffix.
-    $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
-  }
+
+function bartik_c2s_preprocess_page(&$variables) {
+    if (user_is_logged_in() == FALSE && $_GET['q'] != 'user/login') {
+        return drupal_goto('user/login');
+    }
 }
 
 /**
  * Implements hook_preprocess_maintenance_page().
  */
-function bartik_preprocess_maintenance_page(&$variables) {
-  // By default, site_name is set to Drupal if no db connection is available
-  // or during site installation. Setting site_name to an empty string makes
-  // the site and update pages look cleaner.
-  // @see template_preprocess_maintenance_page
-  if (!$variables['db_is_active']) {
-    $variables['site_name'] = '';
-  }
-  drupal_add_css(drupal_get_path('theme', 'bartik') . '/css/maintenance-page.css');
-}
-
-/**
- * Override or insert variables into the maintenance page template.
- */
-function bartik_process_maintenance_page(&$variables) {
-  // Always print the site name and slogan, but if they are toggled off, we'll
-  // just hide them visually.
-  $variables['hide_site_name']   = theme_get_setting('toggle_name') ? FALSE : TRUE;
-  $variables['hide_site_slogan'] = theme_get_setting('toggle_slogan') ? FALSE : TRUE;
-  if ($variables['hide_site_name']) {
-    // If toggle_name is FALSE, the site_name will be empty, so we rebuild it.
-    $variables['site_name'] = filter_xss_admin(variable_get('site_name', 'Drupal'));
-  }
-  if ($variables['hide_site_slogan']) {
-    // If toggle_site_slogan is FALSE, the site_slogan will be empty, so we rebuild it.
-    $variables['site_slogan'] = filter_xss_admin(variable_get('site_slogan', ''));
-  }
-}
+//function bartik_c2s_preprocess_maintenance_page(&$variables) {
+//    // By default, site_name is set to Drupal if no db connection is available
+//    // or during site installation. Setting site_name to an empty string makes
+//    // the site and update pages look cleaner.
+//    // @see template_preprocess_maintenance_page
+//    if (!$variables['db_is_active']) {
+//        $variables['site_name'] = '';
+//    }
+//    drupal_add_css(drupal_get_path('theme', 'ipca_portal') . '/css/maintenance-page.css');
+//}
 
 /**
  * Override or insert variables into the node template.
  */
-function bartik_preprocess_node(&$variables) {
-  if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
-    $variables['classes_array'][] = 'node-full';
-  }
+function bartik_c2s_preprocess_node(&$variables) {
+    if ($variables['view_mode'] == 'full' && node_is_page($variables['node'])) {
+        $variables['classes_array'][] = 'node-full';
+    }
 }
 
 /**
  * Override or insert variables into the block template.
  */
-function bartik_preprocess_block(&$variables) {
-  // In the header region visually hide block titles.
-  if ($variables['block']->region == 'header') {
-    $variables['title_attributes_array']['class'][] = 'element-invisible';
-  }
+function bartik_c2s_preprocess_block(&$variables) {
+    // In the header region visually hide block titles.
+    if ($variables['block']->region == 'header') {
+        $variables['title_attributes_array']['class'][] = 'element-invisible';
+    }
 }
 
-/**
- * Implements theme_menu_tree().
- */
-function bartik_menu_tree($variables) {
-  return '<ul class="menu clearfix">' . $variables['tree'] . '</ul>';
+
+
+function bartik_c2s_form_alter(&$form, $form_state, $form_id) {
+    switch ($form_id) {
+        case 'user_login_block':
+        case 'user_login':
+            unset($form['links']);
+            $form['name']['#description'] = t('');
+            $form['pass']['#description'] = t('');            
+            break;
+    }
 }
 
-/**
- * Implements theme_field__field_type().
- */
-function bartik_field__taxonomy_term_reference($variables) {
-  $output = '';
+function bartik_c2s_user_login(&$edit, $account) {
+  //$edit['redirect'] = '<front>';
+  $GLOBALS['destination'] = '<front>';
+}
 
-  // Render the label, if it's not hidden.
-  if (!$variables['label_hidden']) {
-    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
-  }
+function bartik_c2s_css_alter(&$css) {
 
-  // Render the items.
-  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
-  foreach ($variables['items'] as $delta => $item) {
-    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
-  }
-  $output .= '</ul>';
+    unset($css[drupal_get_path('module', 'support') . '/css/main.css']);
+    unset($css[drupal_get_path('module', 'pms_dashboard') . '/css/pms_dashboard.css']);
+    unset($css[drupal_get_path('module', 'attendance') . '/css/attendance.css']);
+    unset($css[drupal_get_path('module', 'system') . '/system.menus.css']);
+    unset($css[drupal_get_path('module', 'system') . '/system.theme.css']);
+    unset($css[drupal_get_path('module', 'lightbox2') . '/css/lightbox.css']);
+}
 
-  // Render the top-level DIV.
-  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '"' . $variables['attributes'] .'>' . $output . '</div>';
+function bartik_c2s_js_alter(&$js) {
 
-  return $output;
+    unset($js[drupal_get_path('module', 'candidate') . '/js/candidate.js']);
+    unset($js[drupal_get_path('module', 'employee_deputation') . '/js/employee_deputation.js']);
+    unset($js[drupal_get_path('module', 'employee_induction') . '/js/pms.js']);
+    unset($js[drupal_get_path('module', 'employee_transfer') . '/js/employee_transfer.js']);
+    unset($js[drupal_get_path('module', 'manpower_requisition') . '/js/organogram.js']);
+    unset($js[drupal_get_path('module', 'salary_structure') . '/js/salary_structure.js']);
+    unset($js[drupal_get_path('module', 'support') . '/js/field_validations.js']);
+    unset($js[drupal_get_path('module', 'support') . '/js/candidate.js']);
+    unset($js[drupal_get_path('module', 'support') . '/js/main_menu.js']);
+    unset($js[drupal_get_path('module', 'visitor_management') . '/js/visitor_management.js']);
+    unset($js[drupal_get_path('module', 'lightbox2') . '/js/lightbox.js']);
+    unset($js[drupal_get_path('module', 'colorbox') . '/js/jquery.colorbox-min.js']);
+    unset($js[drupal_get_path('module', 'colorbox') . '/js/colorbox.js']);
+    unset($js[drupal_get_path('module', 'colorbox') . '/js/colorbox_load.js']);
+   
+    
+    
+}
+
+function bartik_c2s_theme() {
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+  'render element' => 'form',
+  'path' => drupal_get_path('theme', 'ipca_portal') . '/templates',
+  'template' => 'user-login',
+  
+ );
+return $items;
 }
