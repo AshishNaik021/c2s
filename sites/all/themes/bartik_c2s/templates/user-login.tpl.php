43d27f5900d15df5c81463e4f613b3cc58bee7e8
amanpathak05 <pathakaman05@gmail.com>
2015-09-15 14:58:31 +0530

    login changes

---
+++ sites/all/themes/bartik_c2s/templates/user-login.tpl.php
@@ -0,0 +1,122 @@
+<meta content="IE=Edge" http-equiv="X-UA-Compatible">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<link type="image/vnd.microsoft.icon" href="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/favicon.ico" rel="shortcut icon">
+<meta content="Drupal 7 (http://drupal.org)" name="Generator">
+<title>Welcome to City2Shore | City2Shore</title>
+<style media="all" type="text/css">
+<style media="all" type="text/css">
+<style media="all" type="text/css">
+<style media="all" type="text/css">
+<style media="print" type="text/css">
+<script src="http://localhost/C2S_Drupal/misc/jquery.js?v=1.4.4" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/misc/jquery.once.js?v=1.2" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/misc/drupal.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/modules/captcha/captcha.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/jquery-1.8.2.min.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/jquery-ui.custom.min.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/fullcalendar.min.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/gcal.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/jquery.autocomplete.js?nuimwc" type="text/javascript">
+<script src="http://localhost/C2S_Drupal/sites/all/themes/bartik_c2s/js/custom.js?nuimwc" type="text/javascript">
+<script type="text/javascript">
+</head>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+ <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<body>
+        <div id="header">
+		</div>
+		       <div id="content_wrapper">
+			     <div class="content">
+				   <div class="content1">
+			          <div class="logo">
+			          
+			             <img class="img" src="images/logo.png" alt="logoIcon">
+			        </div>
+			            <h3 class="title1"> Login </h3>
+			
+			           <div class="search">
+					        <div class="search1"><input type="text" id="myInput" placeholder="Email / User name"/>
+			                    
+								<span class="fa fa-envelope"></span>
+                            </div>
+			           </div>
+			           <div class="search">
+					        <div class="search1"><input type="text" id="myInput" placeholder="password"/>
+			                   
+								<span class="fa fa-lock"></span>
+                            </div>
+			          </div>
+			           <div class="captcha">
+                              <img class="img1" src="images/images.jpeg" alt="captchaIcon"> 
+							<h6 class="title2">Type the two words seperated by a space(not case sensetive)</h6>
+			           </div>
+			           <div class="button">
+			              <h3 class="title3"> forget password </h3>
+			              <button class="b1" type="button">Login</button>
+						</div>  
+			     </div>
+				</div> 
+			</div>
+             			
+			  
+</body>
+            
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
+									<div class="front_leftsidebar">
+                                        <div class="front_user_login_box">
+                                                        <div class="front_user_login_ipca">
+                                                                <div class="front_user_login">Use Login</div>
+                                                                <div class="front_bartik_c2s_logo"><img src="../sites/all/themes/bartik_c2s/images/IPCA-login-logo.jpg" /></div>
+                                                        </div>
+
+                                             <?php /*
+                    
+                                                    // print_r($ulogin);
+                                                        $form['pass']['#title']=$form['name']['#title']='';
+                                                        $form['pass']['#required']=$form['name']['#required']=FALSE;
+                                                        $form['pass']['#theme_wrappers']=$form['name']['#theme_wrappers']=array();
+                                                        $form['pass']['#attributes']=array('placeholder'=>'Password');
+                                                        $form['name']['#attributes']=array('placeholder'=>'User ID');
+
+                                        //print $log; ?> 
+                                                <div class="ipca_user_main">
+                                                        <div class="user_image">&nbsp;</div>
+                                                        <div class="ipca_user_input"><?php print drupal_render($form['name']);?></div>
+                                                </div> 
+                                                        <br>
+                                                 <div class="ipca_user_main">
+                                                        <div class="user_password">&nbsp;</div>
+                                                        <div class="ipca_user_input"><?php print drupal_render($form['pass']);?></div>
+                                                 </div>
+                                                        <div class="forgot_pass"><a href="">Forgot your password?</a></div>
+                                                        <input type="submit" id="edit-submit" name="op" value="Login" class="form-submit">
+                                                        <?php                                            
+                                                        // print drupal_render($form['actions']);
+                                                        print drupal_render($form['form_build_id']);
+                                                        print drupal_render($form['form_id']);
+														
+														*/
+                                                        ?>
+                                                      
+                                        </div>
+                                </div>
+                
+                
+                         -->
+                
+
