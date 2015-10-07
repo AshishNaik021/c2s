e349a505b312c79c9ed7582411a1aad6be51edd1
AshleeMunshi <ashleshaabhyankar@Ashleshas-Mac-mini.local>
2015-10-07 17:33:23 +0530

    commit by Ashlee -- 7oct15

--- sites/all/modules/c2s/broker/js/broker.js
+++ sites/all/modules/c2s/broker/js/broker.js
@@ -29,17 +29,8 @@ jQuery(document).ready(function(){
 	});
 	
 	//-------//
-	jQuery('.form_section_tab').click(function(){
-		section = $(this).attr('data-item');
-		$('.form_sections').hide();
-		//$('.form_section_tab').find('.selected').removeClass('form_section_tab selected').addClass('form_section_tab');
-		$('.selected').removeClass('form_section_tab selected').addClass('form_section_tab');
-		$(this).addClass('selected');
-		$('#'+section).slideToggle(500,function(){
-//			$('#'+section+'_arrow').toggleClass("icon_rotate");
-		});
-		
-	});
+
+
 	
 	
 	
@@ -56,4 +47,29 @@ function removePrefix(obj){
 	if(obj.value.length > 0){
 		obj.value = obj.value.replace("City2Shore ", "");
 	}
+}
+function convert_edit(i){
+	form_id = 'c2s-broker-edit-view';
+	if(i>1){
+		form_id = form_id+'--'+i;
+	}
+	$("#"+form_id).each(function(){
+	    $(this).find('select').removeClass('input_disabled').addClass('select-xlarge uniform form-select'); //<-- Should return all input elements in that specific form.
+	    $(this).find('input').removeClass('input_disabled').addClass('form_text'); //<-- Should return all input elements in that specific form.
+	    
+	});
+}
+
+function click_tabs(obj_id){
+
+		section = $("#"+obj_id).attr('data-item');
+		$('.form_sections').hide();
+		//$('.form_section_tab').find('.selected').removeClass('form_section_tab selected').addClass('form_section_tab');
+		$('.selected').removeClass('form_section_tab selected').addClass('form_section_tab');
+		$("#"+obj_id).addClass('selected');
+		$('#'+section).show();
+		/*$('#'+section).slideToggle(500,function(){
+//			$('#'+section+'_arrow').toggleClass("icon_rotate");
+		});*/
+		
 }
