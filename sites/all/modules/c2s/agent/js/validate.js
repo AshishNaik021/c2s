0c64949a7e8e9e381d8f4f61f52321f1a30623c5
ashishnaik021 <ashish.a.naik@gmail.com>
2015-10-10 15:08:52 +0530

    Charity Module and Agent/js/Validate.js changes

--- sites/all/modules/c2s/agent/js/validate.js
+++ sites/all/modules/c2s/agent/js/validate.js
@@ -7,9 +7,21 @@ function IsNumeric(e, obj) {
     var keyCode = e.which ? e.which : e.keyCode;
     var ret = ((keyCode >= 48 && keyCode <= 57) || specialKeys.indexOf(keyCode) != -1);
     obj.style.border = ret ? "1px solid #d4d4d4" : "2px solid #ff0000";
+    obj.style.background = ret ? "#ffffff" : "#ffefef";
     return ret;
 }
 
+function phoneNumber(e, obj) {
+	var specialKeys = new Array();
+    specialKeys.push(8);  //Backspace
+    specialKeys.push(46); //Delete
+    specialKeys.push(45); //Dash '-'
+    var keyCode = e.which ? e.which : e.keyCode;
+    var ret = ((keyCode >= 48 && keyCode <= 57) || specialKeys.indexOf(keyCode) != -1);
+    obj.style.border = ret ? "1px solid #d4d4d4" : "2px solid #ff0000";
+    obj.style.background = ret ? "#ffffff" : "#ffefef";
+    return ret;
+}
 
 function onlyAlphabets(e, obj) {
 		var specialKeys = new Array();
@@ -18,6 +30,7 @@ function onlyAlphabets(e, obj) {
 		var keyCode = e.which ? e.which : e.keyCode;
 		var ret = ((keyCode > 64 && keyCode < 91) || (keyCode > 96 && keyCode < 123) || specialKeys.indexOf(keyCode) != -1);
 		obj.style.border = ret ? "1px solid #d4d4d4" : "2px solid #ff0000";
+		obj.style.background = ret ? "#ffffff" : "#ffefef";
 	    return ret;
 }
 
@@ -27,23 +40,26 @@ function isAlphaNumeric(e, obj){
 	specialKeys.push(8); //Backspace
 	specialKeys.push(32); //Space
 	var keyCode = e.which ? e.which : e.keyCode;
-	var ret = ((keyCode > 47 && keyCode < 58) || (keyCode > 64 && keyCode < 91) || (keyCode > 96 && keyCode < 123) || specialKeys.indexOf(keyCode) != -1);
+	var ret = ((keyCode > 47 && keyCode < 58) || (keyCode > 64 && keyCode < 91) || (keyCode > 95 && keyCode < 123) || specialKeys.indexOf(keyCode) != -1);
 	obj.style.border = ret ? "1px solid #d4d4d4" : "2px solid #ff0000";
+    obj.style.background = ret ? "#ffffff" : "#ffefef";
     return ret;
    
  } 
 
-function MailId(e, obj)
+function isMailId(e, obj)
 {
 	var specialKeys = new Array();
-	specialKeys.push(8); //Backspace
-	specialKeys.push(32); //Space
-	specialKeys.push(64); //@
-	specialKeys.push(95); //Underscore
+	specialKeys.push(8);   //Backspace
+	specialKeys.push(46);  //Delete
+	specialKeys.push(64);  //@
+	specialKeys.push(95);  //Underscore '_'
+	specialKeys.push(190); //Dot / Period '.'
 	
 	var keyCode = e.which ? e.which : e.keyCode;
-	var ret = ((keyCode > 47 && keyCode < 58) || (keyCode > 64 && keyCode < 91) || (keyCode > 96 && keyCode < 123) || specialKeys.indexOf(keyCode) != -1);
-	obj.style.border = ret ? "none" : "2px solid #ff0000";
+	var ret = ((keyCode > 47 && keyCode < 58) || (keyCode > 64 && keyCode < 91) || (keyCode > 95 && keyCode < 123) || specialKeys.indexOf(keyCode) != -1);
+	obj.style.border = ret ? "1px solid #d4d4d4" : "2px solid #ff0000";
+    obj.style.background = ret ? "#ffffff" : "#ffefef";
     return ret;
 	
 }
